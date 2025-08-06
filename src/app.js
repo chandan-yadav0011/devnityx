c727304191b24d3cc0e846d4f2444521e07e63f7
chandan-yadav0011 <chandanyadav8145@gmail.com>
2025-08-06 14:47:46 +0530

    added cors set up origin and allowed credentials

--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const authRouter = require('./routes/auth');
 const profileRouter = require('./routes/profile');
 const requestRouter = require('./routes/request');
 const userRouter = require("./routes/user");
 app.use(express.json());   // converts the json to javascript object
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true
+}));
 
 app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
-app.use("/",userRouter);
+app.use("/",userRouter);   
 
 
 app.get("/feed",async(req,res)=>{
